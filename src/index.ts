c393fad6996c7367a35c49899b25d8e740a39a54
Lázaro <lazaroyaesta@gmail.com>
2022-07-16 16:36:14 +0200

    ✨ Add Typegoose Model

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
+import UserModel from "./models/User";
 
 async function connectDB() {
-  const db = await mongoose.connect("mongodb://localhost/typesgoosedb");
+  const db = await connect("mongodb://localhost/typesgoosedb");
   console.log("Database is connected to", db.connection.db.databaseName);
 }
 
 connectDB();
+
+async function executeQueries() {
+  const user = new UserModel({
+    firstname: "Joe",
+    lastname: "Doe",
+    email: "joedoe@gmail.com",
+    password: "123456",
+  });
+
+  await user.save();
+  console.log();
+}
---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+import { prop, getModelForClass } from "@typegoose/typegoose";
+
+class User {
+  @prop()
+  firstname: string;
+  @prop()
+  lastname: string;
+  @prop()
+  email: string;
+  @prop()
+  password: string;
+}
+
+const UserModel = getModelForClass(User);
+
+export default UserModel;
